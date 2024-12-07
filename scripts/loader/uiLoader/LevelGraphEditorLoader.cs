88fbc942d0fd4f3c216c969f9ad2aaa46248bff6
Cold-Mint <cold_mint@qq.com>
2024-12-07 11:35:34 +0800

    Refactor file operations for async task handling. 为异步任务处理重构文件操作。

--- scripts/loader/uiLoader/LevelGraphEditorLoader.cs
+++ scripts/loader/uiLoader/LevelGraphEditorLoader.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Threading.Tasks;
 using ColdMint.scripts.debug;
 using ColdMint.scripts.levelGraphEditor;
 using ColdMint.scripts.nodeBinding;
@@ -452,7 +453,7 @@ public partial class LevelGraphEditorLoader : UiLoaderTemplate
 
         if (_nodeBinding.ActionButton != null)
         {
-            _nodeBinding.ActionButton.Pressed += () =>
+            _nodeBinding.ActionButton.Pressed += async () =>
             {
                 if (_nodeBinding.FileNameLineEdit == null)
                 {
@@ -467,12 +468,12 @@ public partial class LevelGraphEditorLoader : UiLoaderTemplate
 
                 if (_saveMode)
                 {
-                    SaveFile(fileName);
+                    await SaveFile(fileName);
                 }
 
                 else
                 {
-                    LoadFile(fileName);
+                    await LoadFile(fileName);
                 }
             };
         }
@@ -487,102 +488,95 @@ public partial class LevelGraphEditorLoader : UiLoaderTemplate
     ///<para>filename</para>
     ///<para>文件名</para>
     /// </param>
-    private async void SaveFile(string fileName)
+    private async Task SaveFile(string fileName)
     {
-        try
+        if (_nodeBinding.GraphEdit == null)
         {
-            if (_nodeBinding.GraphEdit == null)
+            return;
+        }
+
+        var levelGraphEditorSaveData = new LevelGraphEditorSaveData();
+        //Serialize room node information
+        //序列化房间节点信息
+        var length = _nodeBinding.GraphEdit.GetChildCount();
+        if (length <= 0)
+        {
+            //no room
+            //没有房间
+            return;
+        }
+
+        var roomNodeDataList = new List<RoomNodeData>();
+        levelGraphEditorSaveData.RoomNodeDataList = roomNodeDataList;
+        for (var i = 0; i < length; i++)
+        {
+            var node = _nodeBinding.GraphEdit.GetChild(i);
+            if (node is not RoomNode roomNode) continue;
+            var data = roomNode.RoomNodeData;
+            if (data == null)
             {
-                return;
+                continue;
             }
 
-            var levelGraphEditorSaveData = new LevelGraphEditorSaveData();
-            //Serialize room node information
-            //序列化房间节点信息
-            var length = _nodeBinding.GraphEdit.GetChildCount();
-            if (length <= 0)
-            {
-                //no room
-                //没有房间
-                return;
-            }
+            roomNodeDataList.Add(data);
+        }
 
-            var roomNodeDataList = new List<RoomNodeData>();
-            levelGraphEditorSaveData.RoomNodeDataList = roomNodeDataList;
-            for (var i = 0; i < length; i++)
+        //Serialized connection information
+        //序列化连接信息
+        Array<Dictionary> connectionList = _nodeBinding.GraphEdit.GetConnectionList();
+        var connectionDataList = new List<ConnectionData>();
+        levelGraphEditorSaveData.ConnectionDataList = connectionDataList;
+        if (connectionList.Count > 0)
+        {
+            foreach (var dictionary in connectionList)
             {
-                var node = _nodeBinding.GraphEdit.GetChild(i);
-                if (node is not RoomNode roomNode) continue;
-                var data = roomNode.RoomNodeData;
-                if (data == null)
+                var keys = dictionary.Keys;
+                if (keys.Count == 0)
                 {
                     continue;
                 }
 
-                roomNodeDataList.Add(data);
-            }
-
-            //Serialized connection information
-            //序列化连接信息
-            Array<Dictionary> connectionList = _nodeBinding.GraphEdit.GetConnectionList();
-            var connectionDataList = new List<ConnectionData>();
-            levelGraphEditorSaveData.ConnectionDataList = connectionDataList;
-            if (connectionList.Count > 0)
-            {
-                foreach (var dictionary in connectionList)
+                var connectionData = new ConnectionData();
+                foreach (var variant in keys)
                 {
-                    var keys = dictionary.Keys;
-                    if (keys.Count == 0)
+                    var typeStr = variant.ToString();
+                    switch (typeStr)
                     {
-                        continue;
+                        case "from_node":
+                            var fromRoomNodeData = GetRoomNodeData(dictionary[variant].AsString());
+                            if (fromRoomNodeData == null)
+                            {
+                                continue;
+                            }
+
+                            connectionData.FromId = fromRoomNodeData.Id;
+                            break;
+                        case "from_port":
+                            connectionData.FromPort = dictionary[variant].AsInt32();
+                            break;
+                        case "to_node":
+                            var toRoomNodeData = GetRoomNodeData(dictionary[variant].AsString());
+                            if (toRoomNodeData == null)
+                            {
+                                continue;
+                            }
+
+                            connectionData.ToId = toRoomNodeData.Id;
+                            break;
+                        case "to_port":
+                            connectionData.ToPort = dictionary[variant].AsInt32();
+                            break;
+                        default:
+                            throw new InvalidOperationException($"Unexpected key: {typeStr}");
                     }
-
-                    var connectionData = new ConnectionData();
-                    foreach (var variant in keys)
-                    {
-                        var typeStr = variant.ToString();
-                        switch (typeStr)
-                        {
-                            case "from_node":
-                                var fromRoomNodeData = GetRoomNodeData(dictionary[variant].AsString());
-                                if (fromRoomNodeData == null)
-                                {
-                                    continue;
-                                }
-
-                                connectionData.FromId = fromRoomNodeData.Id;
-                                break;
-                            case "from_port":
-                                connectionData.FromPort = dictionary[variant].AsInt32();
-                                break;
-                            case "to_node":
-                                var toRoomNodeData = GetRoomNodeData(dictionary[variant].AsString());
-                                if (toRoomNodeData == null)
-                                {
-                                    continue;
-                                }
-
-                                connectionData.ToId = toRoomNodeData.Id;
-                                break;
-                            case "to_port":
-                                connectionData.ToPort = dictionary[variant].AsInt32();
-                                break;
-                            default:
-                                throw new InvalidOperationException($"Unexpected key: {typeStr}");
-                        }
-                    }
-
-                    connectionDataList.Add(connectionData);
                 }
-            }
 
-            var filePath = Path.Join(Config.GetLevelGraphExportDirectory(), FileNameToActualName(fileName));
-            await File.WriteAllTextAsync(filePath, YamlSerialization.Serialize(levelGraphEditorSaveData));
+                connectionDataList.Add(connectionData);
+            }
         }
-        catch (Exception e)
-        {
-            LogCat.WhenCaughtException(e);
-        }
+
+        var filePath = Path.Join(Config.GetLevelGraphExportDirectory(), FileNameToActualName(fileName));
+        await File.WriteAllTextAsync(filePath, YamlSerialization.Serialize(levelGraphEditorSaveData));
     }
 
     /// <summary>
@@ -610,87 +604,80 @@ public partial class LevelGraphEditorLoader : UiLoaderTemplate
     }
 
 
-    private async void LoadFile(string fileName)
+    private async Task LoadFile(string fileName)
     {
-        try
+        if (_nodeBinding.GraphEdit == null)
         {
-            if (_nodeBinding.GraphEdit == null)
-            {
-                return;
-            }
+            return;
+        }
 
-            var filePath = Path.Join(Config.GetLevelGraphExportDirectory(), FileNameToActualName(fileName));
-            if (!File.Exists(filePath))
-            {
-                //file does not exist
-                //文件不存在。
-                return;
-            }
+        var filePath = Path.Join(Config.GetLevelGraphExportDirectory(), FileNameToActualName(fileName));
+        if (!File.Exists(filePath))
+        {
+            //file does not exist
+            //文件不存在。
+            return;
+        }
 
-            var levelGraphEditorSaveData =
-                await YamlSerialization.ReadYamlFileToObj<LevelGraphEditorSaveData>(filePath);
-            if (levelGraphEditorSaveData == null)
-            {
-                //Deserialization failed.
-                //反序列化失败。
-                return;
-            }
+        var levelGraphEditorSaveData =
+            await YamlSerialization.ReadYamlFileToObj<LevelGraphEditorSaveData>(filePath);
+        if (levelGraphEditorSaveData == null)
+        {
+            //Deserialization failed.
+            //反序列化失败。
+            return;
+        }
 
-            //Do not call DeleteAllChildAsync; this will raise "ERROR: Caller thread can't call this function in this node."
-            //不要调用DeleteAllChildAsync方法，这会引发“ERROR: Caller thread can't call this function in this node”。
-            NodeUtils.DeleteAllChild(_nodeBinding.GraphEdit);
-            _roomIndex = 1;
-            _hasStartNode = false;
-            var roomNodeDataList = levelGraphEditorSaveData.RoomNodeDataList;
-            if (roomNodeDataList != null)
+        //Do not call DeleteAllChildAsync; this will raise "ERROR: Caller thread can't call this function in this node."
+        //不要调用DeleteAllChildAsync方法，这会引发“ERROR: Caller thread can't call this function in this node”。
+        NodeUtils.DeleteAllChild(_nodeBinding.GraphEdit);
+        _roomIndex = 1;
+        _hasStartNode = false;
+        var roomNodeDataList = levelGraphEditorSaveData.RoomNodeDataList;
+        if (roomNodeDataList != null)
+        {
+            foreach (var roomNodeData in roomNodeDataList)
             {
-                foreach (var roomNodeData in roomNodeDataList)
+                if (string.IsNullOrEmpty(roomNodeData.Id))
                 {
-                    if (string.IsNullOrEmpty(roomNodeData.Id))
-                    {
-                        continue;
-                    }
-
-                    if (!_hasStartNode && roomNodeData.HasTag(Config.RoomDataTag.StartingRoom))
-                    {
-                        _hasStartNode = true;
-                    }
-
-                    //Instantiate the room node.
-                    //实例化房间节点。
-                    var roomNode = CreateRoomNode(roomNodeData);
-                    if (roomNode != null)
-                    {
-                        roomNode.Name = roomNodeData.Id;
-                    }
+                    continue;
                 }
-            }
 
-            var connectionDataList = levelGraphEditorSaveData.ConnectionDataList;
-            if (connectionDataList != null)
-            {
-                foreach (var connectionData in connectionDataList)
+                if (!_hasStartNode && roomNodeData.HasTag(Config.RoomDataTag.StartingRoom))
                 {
-                    if (string.IsNullOrEmpty(connectionData.FromId))
-                    {
-                        continue;
-                    }
+                    _hasStartNode = true;
+                }
 
-                    if (string.IsNullOrEmpty(connectionData.ToId))
-                    {
-                        continue;
-                    }
-
-                    //Connecting rooms
-                    //连接房间
-                    _nodeBinding.GraphEdit?.ConnectNode(connectionData.FromId, connectionData.FromPort, connectionData.ToId,
-                        connectionData.ToPort);
+                //Instantiate the room node.
+                //实例化房间节点。
+                var roomNode = CreateRoomNode(roomNodeData);
+                if (roomNode != null)
+                {
+                    roomNode.Name = roomNodeData.Id;
                 }
             }
         }
-        catch (Exception e)
+
+        var connectionDataList = levelGraphEditorSaveData.ConnectionDataList;
+        if (connectionDataList != null)
         {
-            LogCat.WhenCaughtException(e);
+            foreach (var connectionData in connectionDataList)
+            {
+                if (string.IsNullOrEmpty(connectionData.FromId))
+                {
+                    continue;
+                }
+
+                if (string.IsNullOrEmpty(connectionData.ToId))
+                {
+                    continue;
+                }
+
+                //Connecting rooms
+                //连接房间
+                _nodeBinding.GraphEdit?.ConnectNode(connectionData.FromId, connectionData.FromPort, connectionData.ToId,
+                    connectionData.ToPort);
+            }
         }
     }
 
